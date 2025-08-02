fb02c97c8e9a998258e77fcc72b2b0669aea620f
mosaddek75 <mosaddekmuttakey4390.com>
2025-08-02 13:23:19 +0600

    Task Add

---
+++ Mid/Task_1/View/from.php
@@ -0,0 +1,76 @@
+<DOCTYPE html>
+    <html lang ="en">
+    <head>
+    <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>This is Registration from</title>
+     </head>
+    <body>
+      <center>
+
+     <h1>AIUB</h1>     
+     <h2>Course registration From<h2>
+     </center>
+    <h3 align="left">Start the registration and fill the from</h3>
+
+  <table>
+<tr>
+<td>Fullname:</td>
+<td><input type="text"></td>
+</tr>
+<tr>
+<td>Email:</td>
+<td><input type="text"></td>
+</tr>
+<tr>
+<td>Password:</td>
+<td><input type="text"></td>
+</tr>
+<tr>
+<td> Select the Gender:</td>
+<td>
+<input type="radio" name="gender">Male
+<input type="radio" name="gender">Other
+</td>
+</tr>
+<tr>
+<td>Languages Known:</td>
+<td>
+<input type ="checkbox"name="lang">English
+<input type ="checkbox"name="lang">Bangla
+<input type ="checkbox"name="lang">Hindi
+</td>
+</tr>
+<tr> 
+<td>Upload your Photo:</td>
+<td><input type="file"></td>
+</tr>
+<tr> 
+<td>Date of Birth</td>
+<td><input type="date"></td>
+</tr>
+<tr>
+<td>Country Name :</td>
+<td>
+<select>
+<option value=""> -- Select Option
+<option value=""> -- Bangladesh
+<option value=""> -- Bhutan
+<option value=""> -- Napal
+</td>
+</tr>
+<tr> <td>
+Comments :</td>
+<td><textarea rows="4" cols="40"></textarea></td>
+<tr> <td>
+<td><input type="Submit" value="Register"></td>
+</tr>
+
+ </table>
+
+
+
+</body>
+</html>
+
+
---
+++ Mid/Task_2/view/bank.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+    <html lang ="en">
+    <head>
+    <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>This is Registration from</title>
+     </head>
+    <body Style="background-color: #6687dbff; font-family: Arial, sans-serif;">
+      <center>
+
+     <h1 Style = "Color: Red;">Bank Management System</h1>     
+     <h2 style = "color: Blue;">Your Trusted Financial Partner<h2>
+     </center>
+    <h3 align="left"> Customer Registration From</h3>
+
+  <table style=" background-color: #f2f2f2; padding: 10px; border-radius: 5px;">
+<tr>
+<td>Fullname:</td>
+<td><input type="text"></td>
+</tr>
+
+<tr> 
+<td>Date of Birth</td>
+<td><input type="date"></td>
+</tr>
+
+<tr>
+<td> Select the Gender:</td>
+<td>
+<input type="radio" name="gender">Male
+<input type="radio" name="gender">Femaile
+<input type="radio" name="gender">Other
+</td>
+</tr>
+
+<tr>
+<td>Marital Status :</td>
+<td>
+<select>
+<option value=""> -- Single
+<option value=""> -- Married
+<option value=""> -- Divorced
+<option value=""> -- Widowed
+</td>
+</tr>
+
+<tr>
+<td>Account Type :</td>
+<td>
+<select>
+<option value=""> -- saving
+<option value=""> -- current
+<option value=""> -- fixed deposit
+<option value=""> -- recurring deposit
+<option value=""> -- loan
+<option value=""> -- credit card
+<option value=""> -- debit card
+<option value=""> -- joint account
+</td>
+</tr>
+
+<tr>
+<td>Initial Deposit Amount :</td>
+<td><input type="number"></td>
+</tr>
+
+<tr>
+<td>Mobile Number :</td>
+<td><input type="Number"></td>
+</tr>
+
+<tr>
+<td>Email Address :</td>
+<td><input type="text"></td>
+</tr>
+
+<tr>
+<td>Address :</td>
+<td><input type="text"></td>
+</tr>
+
+
+<tr>
+<td>Occupation:</td>
+<td><input type="text"></td>
+</tr>
+
+<tr>
+<td>National ID (NID):</td>
+<td><input type="number"></td>
+</tr>
+
+<tr>
+<td> Set Password :</td>
+<td><input type="text"></td>
+</tr>
+
+<tr> 
+<td>Upload ID Proof :</td>
+<td><input type="file"></td>
+</tr>
+
+<tr>
+<td>
+<input type ="checkbox"name="lang"> I agree to the terms and conditions
+</td>
+</tr>
+
+<tr>
+  <td></td>
+<td>
+    <input type="submit" value="Register" Style="background-color: #4c66afff; color: white; padding: 5px;">
+    <input type="reset" value="clear" style="background-color: #4c66afff; color: white; padding: 5px;">
+
+
+ </table>
+</body>
+</html>
+
+
